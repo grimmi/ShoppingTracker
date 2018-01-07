9dd7359ef5d661b8b09a82b01b2bbe3c7d5b5afa
Arne Lehmann <arne.lehmann@gmx.de>
2018-01-07 12:22:05 +0100

    first implementation of azure ocr service

---
+++ ShoppingTracker/AzureJsonResponse.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AzureJsonResponse
+{
+    [JsonProperty("language")]
+    public string Language { get; set; }
+    [JsonProperty("textAngle")]
+    public float TextAngle { get; set; }
+    [JsonProperty("orientation")]
+    public string Orientation { get; set; }
+    [JsonProperty("regions")]
+    public Region[] Regions { get; set; }
+
+    public IEnumerable<Word> Words => Regions.SelectMany(region => region.Lines.SelectMany(line => line.Words));
+}
+
+public class Region
+{
+    [JsonProperty("boundingBox")]
+    public string BoundingBox { get; set; }
+    [JsonProperty("lines")]
+    public Line[] Lines { get; set; }
+}
+
+public class Line
+{
+    [JsonProperty("boundingBox")]
+    public string BoundingBox { get; set; }
+    [JsonProperty("words")]
+    public Word[] Words { get; set; }
+}
+
+public class Word
+{
+    [JsonProperty("boundingBox")]
+    public string BoundingBox { get; set; }
+    [JsonProperty("text")]
+    public string Text { get; set; }
+}
---
+++ ShoppingTracker/AzureOcrService.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace ShoppingTracker
+{
+    public class AzureOcrService : IOcrService
+    {
+        private string _apiKey;
+        private string _uriBase;
+        private HttpClient _client;
+
+        public AzureOcrService(string apiKey, string uriBase, HttpClient client)
+        {
+            _apiKey = apiKey;
+            _uriBase = uriBase;
+            _client = client;
+        }
+
+        public async Task<OcrResult> Process(byte[] image)
+        {
+            _client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _apiKey);
+
+            var uri = $"{_uriBase}?language=unk&detectOrientation=true";
+
+            using (var content = new ByteArrayContent(image))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                var response = await _client.PostAsync(uri, content);
+
+                var ocrResponse = JsonConvert.DeserializeObject<AzureJsonResponse>(await response.Content.ReadAsStringAsync());
+
+                var words = new List<OcrArea>();
+                foreach (var word in ocrResponse.Words)
+                {
+                    var boundingBox = word.BoundingBox.Split(',').Select(v => int.Parse(v)).ToArray();
+                    var rectangle = new Rectangle(boundingBox[0], boundingBox[1], boundingBox[2] - boundingBox[0], boundingBox[3] - boundingBox[1]);
+                    words.Add(new OcrArea { BoundingBox = rectangle, Text = word.Text });
+                }
+                return new OcrResult { Areas = words, Texts = words.Select(w => w.Text).ToList() };
+            }
+        }
+    }
+}
