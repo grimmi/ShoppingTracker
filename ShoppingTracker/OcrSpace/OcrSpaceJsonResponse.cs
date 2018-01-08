ca4608ad67da4957581eb15cb1510bc064e06fae
Arne Lehmann <arne.lehmann@gmx.de>
2018-01-08 21:15:23 +0100

    working base implementation for ocr.space

---
+++ ShoppingTracker/OcrSpace/OcrSpaceJsonResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace ShoppingTracker
+{
+    public class OcrSpaceResult
+    {
+        public Parsedresult[] ParsedResults { get; set; }
+        public int OCRExitCode { get; set; }
+        public bool IsErroredOnProcessing { get; set; }
+        public object ErrorMessage { get; set; }
+        public object ErrorDetails { get; set; }
+        public string ProcessingTimeInMilliseconds { get; set; }
+        public string SearchablePDFURL { get; set; }
+    }
+
+    public class Parsedresult
+    {
+        public Textoverlay TextOverlay { get; set; }
+        public int FileParseExitCode { get; set; }
+        public string ParsedText { get; set; }
+        public string ErrorMessage { get; set; }
+        public string ErrorDetails { get; set; }
+    }
+
+    public class Textoverlay
+    {
+        public string[] Lines { get; set; }
+        public bool HasOverlay { get; set; }
+        public string Message { get; set; }
+    }
+}
--- ShoppingTracker/OcrSpace/OcrSpaceService.cs
+++ ShoppingTracker/OcrSpace/OcrSpaceService.cs
@@ -1,20 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ShoppingTracker
 {
-    public class OcrSpaceService : IOcrService
+    class ObservableStringContent : StringContent
     {
-        public OcrSpaceService(string apiKey)
+        public ObservableStringContent(string content) : base(content)
         {
-
         }
 
-        public Task<OcrResult> Process(byte[] image)
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
         {
-            throw new NotImplementedException();
+            return Task.Run(() =>
+            {
+                using (var content = base.ReadAsStreamAsync().Result)
+                {
+                    var buffer = new byte[512];
+                    int length;
+
+                    while ((length = content.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        stream.Write(buffer, 0, length);
+                        stream.Flush();
+                    }
+                }
+            });
+        }
+    }
+
+    public class OcrSpaceService : IOcrService
+    {
+        private static string apiUrl = "https://api.ocr.space/parse/image";
+        private string _apiKey;
+
+        public OcrSpaceService(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public async Task<OcrResult> Process(byte[] image)
+        {
+            using (var content = new MultipartFormDataContent())
+            {
+                var base64Image = Convert.ToBase64String(image);
+                content.Add(new StringContent(_apiKey), "apikey");
+                content.Add(new ObservableStringContent("data:image/jpeg;base64," + base64Image), "base64Image");
+
+                using (var client = new HttpClient())
+                using (var response = await client.PostAsync(apiUrl, content))
+                {
+                    var responseText = await response.Content.ReadAsStringAsync();
+                    var ocrResponse = JsonConvert.DeserializeObject<OcrSpaceResult>(responseText);
+                    return new OcrResult
+                    {
+                        Areas = Enumerable.Empty<OcrArea>(),
+                        Texts = ocrResponse.ParsedResults.First().ParsedText.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                    };
+                }
+            }
         }
     }
 }
