9d8b5027638f48aab6dca55a131fb33367836677
Arne Lehmann <arne.lehmann@gmx.de>
2018-01-08 20:56:28 +0100

    started implementing ocr.space service

---
+++ ShoppingTracker/OcrSpace/OcrSpaceService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingTracker
+{
+    public class OcrSpaceService : IOcrService
+    {
+        public OcrSpaceService(string apiKey)
+        {
+
+        }
+
+        public Task<OcrResult> Process(byte[] image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
