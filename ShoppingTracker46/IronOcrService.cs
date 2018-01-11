1410aba4b083f5f3b28210e83d1e8c69093a9da0
Arne Lehmann <arne.lehmann@gmx.de>
2018-01-11 21:13:01 +0100

    added new project targeting .net 4.6 because ironocr doesn't support .net standard

---
+++ ShoppingTracker46/IronOcrService.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Shop = ShoppingTracker;
+using IronOcr;
+using System;
+using System.Linq;
+
+namespace ShoppingTracker46
+{
+    public class IronOcrService : Shop.IOcrService
+    {
+        public Task<Shop.OcrResult> Process(byte[] image)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Shop.OcrResult> Process(string file)
+        {
+            var ocr = new AutoOcr();
+            var result = ocr.Read(file);
+
+            var ocrResult = new Shop.OcrResult()
+            {
+                Areas = Enumerable.Empty<Shop.OcrArea>(),
+                Texts = result.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries )
+            };
+
+            return Task.FromResult(ocrResult);
+        }
+    }
+}
