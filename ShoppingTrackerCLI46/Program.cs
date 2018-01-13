ac411fbf6731f7d88f87526daebd1d72479f6de1
Arne Lehmann <arne.lehmann@gmx.de>
2018-01-13 18:00:24 +0100

    console app for testing

---
+++ ShoppingTrackerCLI46/Program.cs
@@ -0,0 +1,28 @@
+﻿using ShoppingTracker46;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingTrackerCLI46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new IronOcrService();
+            foreach (var file in Directory.EnumerateFiles(@"c:\temp\tesseract"))
+            {
+                var result = service.Process(file).Result;
+                Console.WriteLine("*******************************" + file);
+
+                foreach (var text in result.Texts)
+                {
+                    Console.WriteLine(text);
+                }
+            }
+            Console.WriteLine("all done");
+            Console.ReadKey();
+        }
+    }
+}
